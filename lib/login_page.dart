e5cf7b4e85d6c963cc2a6e1df3c6dc016ed62c44
kawasaki <r.kawasaki@leafnet.jp>
2024-05-02 14:00:49 +0900

    ログイン画面 | 登録ボタン追加

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -23,11 +23,26 @@ class _LoginPageState extends State<LoginPage> {
         title: const Text('ログイン'),
         backgroundColor: Colors.cyan[100],
       ),
-      body: SingleChildScrollView(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Login(),
+          children: <Widget>[
+            isLogin ? Login() : Register(),
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    isLogin = !isLogin;
+                  });
+                },
+                child: isLogin
+                    ? Text(
+                        '新規登録はこちら',
+                        style: TextStyle(decoration: TextDecoration.underline, color: Colors.blue),
+                      )
+                    : Text(
+                        'ログインはこちら',
+                        style: TextStyle(decoration: TextDecoration.underline, color: Colors.redAccent),
+                      )),
           ],
         ),
       ),
@@ -62,6 +77,83 @@ class _LoginPageState extends State<LoginPage> {
             });
           },
         ),
+      ],
+    );
+  }
+
+  Widget Register() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        /// メールアドレス
+        TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'メールアドレス',
+          ),
+          onChanged: (value) {
+            setState(() {
+              email = value;
+            });
+          },
+        ),
+
+        /// パスワード
+        TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'パスワード',
+          ),
+          obscureText: true,
+          onChanged: (value) {
+            setState(() {
+              password = value;
+            });
+          },
+        ),
+
+        /// パスワード確認
+        TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'パスワード確認',
+          ),
+          obscureText: true,
+          onChanged: (value) {
+            setState(() {
+              password1 = value;
+            });
+          },
+        ),
+
+        /// ユーザー名
+        TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'ユーザー名',
+          ),
+          onChanged: (value) {
+            setState(() {
+              name = value;
+            });
+          },
+        ),
+        Text(''),
+        Container(
+          width: double.infinity,
+          // ユーザー登録ボタン
+          child: ElevatedButton(
+            onPressed: () {
+              if (passwordV == password1) {
+                // パスワードが一致した場合
+                print('登録完了');
+                setState(() {
+                  password = password1;
+                });
+              } else {
+                // パスワードが一致しない場合
+                print('パスワードが一致しません');
+              }
+            },
+            child: const Text('新規登録'),
+          ),
+        )
       ],
     );
   }
