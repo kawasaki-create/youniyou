3461588401aee1ccfd3104194e8436ca541c25de
kawasaki <r.kawasaki@leafnet.jp>
2024-05-07 13:29:58 +0900

    友達をリスト表示できるようにした

--- lib/routes/friends_route.dart
+++ lib/routes/friends_route.dart
@@ -15,6 +15,10 @@ class Friends extends HookConsumerWidget {
     final friends = useState([]);
 
     String name = _controller.text;
+
+    // Firestoreからデータを取得するStream
+    final Stream<QuerySnapshot> _friendsStream = FirebaseFirestore.instance.collection('friends').where('user_id', isEqualTo: user?.uid).snapshots();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('友達'),
@@ -81,6 +85,8 @@ class Friends extends HookConsumerWidget {
                                       onPressed: () async {
                                         // データベースに追加
                                         await FirebaseFirestore.instance.collection('friends').add({'name': name, 'icon': '友達のアイコン', 'user_id': user?.uid});
+                                        _controller.clear();
+                                        Navigator.pop(context);
                                       },
                                       child: Text('追加'),
                                     ),
@@ -103,16 +109,39 @@ class Friends extends HookConsumerWidget {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Text('友達'),
-            ),
-            Text(user?.uid ?? '未ログイン'),
-          ],
-        ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _friendsStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+
+          // if (snapshot.connectionState == ConnectionState.waiting) {
+          //   return Center(child: CircularProgressIndicator());
+          // }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(child: Text('友達がいません'));
+          }
+
+          return ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+              return ListTile(
+                leading: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.blue,
+                  ),
+                ),
+                title: Text(data['name']),
+                subtitle: Text(data['user_id']),
+              );
+            }).toList(),
+          );
+        },
       ),
     );
   }
